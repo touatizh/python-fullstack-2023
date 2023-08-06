e8008d328f7e68b6d2616eb1d30d2d5d1c827cf2
Helmi Touati <touatizh@gmail.com>
2023-08-06 20:46:33 +0100

    Updated to include method chaining

--- fundamentals/oop/user.py
+++ fundamentals/oop/user.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 class User:
     """
-    A class that serves as a simple modelization of bank accounts.
+    A class that serves as a simple modelization of bank clients.
 
     Attributes:
             name (str): A string representing the client's name. Mandatory
@@ -30,27 +30,31 @@ class User:
         self.email = email
         self.balance = balance
 
-    def make_deposit(self, amount: int) -> None:
+    def make_deposit(self, amount: int) -> User:
         """
         Adds the given amount to the client's balance.
 
         Args:
             amount (int): A number representing the sum of money that the client wants to deposit.
 
-        Returns: None
+        Returns:
+            self (User): User object with updated balance.
         """        
         self.balance += amount
+        return self
 
-    def make_withdrawal(self, amount: int) -> None:
+    def make_withdrawal(self, amount: int) -> User:
         """
         Substracts the given amount from the client's balance.
 
         Args:
             amount (int): A number representing the sum of money that the client wants to withdraw.
 
-        Returns: None
+        Returns:
+            self (User): User object with updated balance.
         """
         self.balance -= amount
+        return self
 
     def display_user_balance(self) -> None:
         """
@@ -58,7 +62,7 @@ class User:
         """
         print(f"User: {self.name}, Balance: ${self.balance}")
 
-    def transfert_money(self, other_user: User, amount: int) -> None:
+    def transfert_money(self, other_user: User, amount: int) -> User:
         """
         Transfers the given amount between client's accounts.
 
@@ -66,10 +70,12 @@ class User:
             other_user (User): A User type object representing the client that will receive the given amount
             amount (int): A number representing the sum of money to transfer to the given client
 
-        Returns: None
+        Returns:
+            self (User): User object with updated balance.
         """
         self.balance -= amount
         other_user.balance += amount
+        return self
 
 if __name__ == "__main__":
     #Create 3 instances of User
@@ -78,26 +84,20 @@ if __name__ == "__main__":
     user3 = User("Zed Ki")
 
     #user1: 3 deposits and 1 withdrawal
-    user1.make_deposit(300)
-    user1.make_deposit(270)
-    user1.make_deposit(100)
+    user1.make_deposit(300).make_deposit(270).make_deposit(100)
     user1.make_withdrawal(390)
     #user1's balance
     user1.display_user_balance()
 
     #user2: 2 deposits and 2 withdrawals
-    user2.make_deposit(250)
-    user2.make_deposit(1400)
-    user2.make_withdrawal(700)
-    user2.make_withdrawal(230)
+    user2.make_deposit(250).make_deposit(1400)
+    user2.make_withdrawal(700).make_withdrawal(230)
     #user2's balance
     user2.display_user_balance()
 
     #user3: 1 deposit and 3 withdrawls
     user3.make_deposit(700)
-    user3.make_withdrawal(140)
-    user3.make_withdrawal(300)
-    user3.make_withdrawal(350)
+    user3.make_withdrawal(140).make_withdrawal(300).make_withdrawal(350)
     #user3's balance
     user3.display_user_balance()
 
