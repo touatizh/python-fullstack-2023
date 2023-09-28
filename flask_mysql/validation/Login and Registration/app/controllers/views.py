e8dc010df359507b8d2d680b49082824801bd537
Helmi Touati <touatizh@gmail.com>
2023-09-28 12:33:24 +0100

    Refactored view functions
    - Extended the registration process to automatically log in users upon successful registration.
    - Enhanced the login function to redirect users to the dashboard upon successful login.
    - Added the dashboard page.
    - Implemented session management by storing the user ID and checking for its presence for dashboard route.

--- flask_mysql/validation/Login and Registration/app/controllers/views.py
+++ flask_mysql/validation/Login and Registration/app/controllers/views.py
@@ -1,6 +1,8 @@
-from flask import render_template, request, redirect, url_for, session
-from app.controllers.forms import RegForm, LoginForm
+from app.controllers.forms import LoginForm, RegForm
 from app.models.users import User
+
+from flask import flash, redirect, render_template, request, session, url_for
+
 
 def index():
     """
@@ -34,11 +36,42 @@ def register():
         session["form_data"] = request.form
         return redirect(url_for("reg_log.home"))
     
-    User.save(request.form.to_dict())
-    return redirect(url_for("reg_log.home"))
+    session["user_id"] = User.save(request.form.to_dict())
+    return redirect(url_for("reg_log.dashboard"))
 
 def login():
+    """
+    Process the login form submission.
+
+    Validates the login form data. If the data is invalid, it redirects back to the index page. 
+    If the data is valid, it stores the user's ID in the session and redirects to the dashboard.
+
+    Returns:
+        redirect: Redirects to the index page if the form is invalid or the dashboard if the login is successful.
+    """
     form = LoginForm(request.form)
     if not form.validate_on_submit():
         return redirect(url_for("reg_log.home"))
-    return redirect(url_for("reg_log.home"))
+    
+    user = User.get_by_email({"email": request.form.get("email")})
+    session["user_id"] = user.id
+    return redirect(url_for("reg_log.dashboard"))
+
+def dashboard():
+    """
+    Render the dashboard page for the logged-in user.
+
+    Checks if the user is logged in by verifying the presence of user_id in the session.
+    If the user is not logged in, it redirects back to the index page with a flash message.
+    If the user is logged in, it fetches the user's data and renders the dashboard page.
+
+    Returns:
+        render_template: Renders the "dashboard.html" template with the user's first name.
+        redirect: Redirects to the index page if the user is not logged in.
+    """
+    if not session.get("user_id"):
+        flash("Login first. #login", "danger")
+        return redirect(url_for("reg_log.home"))
+    
+    user = User.get_by_id({"id": session["user_id"]})
+    return render_template("dashboard.html", user_name=user.first_name)
